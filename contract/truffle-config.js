ed83cc135bc976f5657242cf72b4ef0dd3557335
Aman <guptaaman200115@gmail.com>
2022-05-14 23:33:09 +0530

    Initial Commit

---
+++ contract/truffle-config.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+module.exports = {
+
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",    // Any network (default: none)
+    },
+    matic_mumbai: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.NETWORK);
+      },
+      network_id: 80001
+    }
+  },
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+      }
+    },
+  },
+};
