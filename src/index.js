fec2eb71ee12dbb19775990c6d4b08726142ccbf
Aman <guptaaman200115@gmail.com>
2022-05-21 17:12:03 +0530

    Adding router and bootstrap

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import Store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
+      <BrowserRouter>
         <App/>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
