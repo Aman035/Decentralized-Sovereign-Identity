2d7c2434d761eb2636cf3baf137574aa30a2563c
Aman <guptaaman200115@gmail.com>
2022-05-21 19:43:48 +0530

    Changing refresh rate to 15 sec

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-export const REFRESH_RATE = 6000;
+export const REFRESH_RATE = 15000;
 export const ALERT_DELAY = 6000;
 export const CONTRACT_ADDRESS = '0xFbD45EFD350dDC7953F3DbEe9B1E5E233b567845';
 export const delay = async() => (
