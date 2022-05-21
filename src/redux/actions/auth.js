fd89c81880bd8be2526b26a56330ade3e8366fb2
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-21 16:41:25 +0530

    Added metamask login and encryption public key registration actions

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,107 @@
+import * as ActionTypes from './actionTypes';
+import detectEthereumProvider from '@metamask/detect-provider'
+import Identity from '../../Identity';
+import { alert } from './alert';
+import { updateUserInfo } from './user';
+import { updateRequestInfo } from './issuerRequest';
+import { updateIssuerInfo } from './issuer';
+import { delay } from '../../helper';
+
+//try connecting to metamask wallet and get account address
+export const trySignin = ()=>async(dispatch)=>{
+
+    dispatch(authLoading());
+
+    let err = "";
+    const provider = await detectEthereumProvider();
+
+    if(provider == null)
+        err = "Metamask Not Installed";
+    else{
+        if(provider !== window.ethereum)
+            err = "We Think Multiple Wallets are connected. Please disable other wallets.";
+    }
+
+    if(err !== ""){
+        dispatch(authError(err));
+        dispatch(alert(err , "error"));
+        return;
+    }
+
+    try{
+        await window.ethereum.request({ method: 'eth_requestAccounts'})
+    }
+    catch(err){
+        dispatch(authError("Can't Connect To MetaMask"));
+        dispatch(alert("Can't Connect To MetaMask" , "error"));
+        return;
+    }
+
+    let account = await window.ethereum.request({ method: 'eth_accounts' });
+
+    try{
+        const newUser = await checkNewUser(account[0]);
+        if(newUser === true){
+            dispatch(alert("New User Account Detected" , "info"));
+            await dispatch(signUp(account[0]));
+        }
+        else{
+            await dispatch(signIn(account[0]));
+        }
+    }
+    catch(err){
+        dispatch(authError(err.message));
+        dispatch(alert(err.message , "error"));
+    }    
+}
+
+//check user is existing or new from blockchain data
+const checkNewUser = async account => {
+    const User = await Identity.methods.UserDetail(account).call();
+    return User.Registered ? false : true;
+}
+
+//register the new user
+const signUp = account => async dispatch => {
+    const publicKey = await window.ethereum.request({
+        method: 'eth_getEncryptionPublicKey',
+        params: [account], // you must have access to the specified account
+    });
+    await Identity.methods.registerUser(publicKey).send({from : account});
+    await dispatch(signIn(account));
+}
+
+//signin user
+const signIn = account => async dispatch => {
+    //Some Fancy Auth Method
+    await dispatch(updateUserInfo(account));
+    dispatch(updateAllInfo(account));
+    dispatch(authSuccess(account));
+    dispatch(alert("Sign In Successfull" , "success"));
+}
+
+const updateAllInfo = account => async dispatch => {
+    await delay();
+    await dispatch(updateUserInfo(account));
+    await dispatch(updateIssuerInfo());
+    await dispatch(updateRequestInfo());
+    dispatch(updateAllInfo(account));
+}
+
+const authLoading = ()=> ({
+    type : ActionTypes.AUTH_LOADING
+}) 
+
+const authSuccess = user => ({
+    type : ActionTypes.AUTH_SUCCESS,
+    user : user
+})
+
+const authError = err => ({
+    type : ActionTypes.AUTH_FAIL,
+    err : err
+}) 
+
+export const authLogout = () => ({
+    type : ActionTypes.AUTH_LOGOUT
+})
