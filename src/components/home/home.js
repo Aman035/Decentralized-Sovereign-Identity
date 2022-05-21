aaa23f40a3d663f041c2e9bea0a4894f534446b4
Aman <guptaaman200115@gmail.com>
2022-05-21 19:16:11 +0530

    Adding home and animations

---
+++ src/components/home/home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './home.css';
+import Animation from './animation';
+import { connect } from 'react-redux';
+import { Button } from '@mui/material';
+import { trySignin } from '../../redux/actions/auth';
+import Design from './design';
+
+const mapStateToProps = state => ({
+    Auth : state.Auth
+}) 
+
+const mapDispatchToProps = dispatch => ({
+    signIn : ()=>dispatch(trySignin())
+})
+
+const Home = props => (
+    <div>
+        <div className="home">
+            <Animation/>
+            <div className="title">
+                <h1 className="homeTitle1">DECENTRA ID</h1>
+                <h4 className="homeTitle2">Identity management solutions for a modern world</h4>
+            </div>
+            {!props.Auth.isAuthenticated?
+                <Button variant = "outlined" color="primary" className="signinBtn" onClick = {props.signIn}>
+                    <h5>Sign In</h5>
+                </Button>
+            :
+            null}
+        </div>
+        <Design/>
+    </div>
+)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
