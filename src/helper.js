64df05d8b72c49fae28c3a01ec5b57740365da30
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-21 15:28:06 +0530

    Added helper.js

---
+++ src/helper.js
@@ -0,0 +1,7 @@
+export const REFRESH_RATE = 6000;
+export const ALERT_DELAY = 6000;
+export const CONTRACT_ADDRESS = '0xFbD45EFD350dDC7953F3DbEe9B1E5E233b567845';
+export const delay = async() => (
+    // return await for better async stack trace support in case of errors.
+    await new Promise(resolve => setTimeout(resolve, REFRESH_RATE))
+)
