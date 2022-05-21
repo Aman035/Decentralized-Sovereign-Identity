172b4ebc83c8224924d5c8a38e784240f5f5a323
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-21 22:26:02 +0530

    Added issuer and not issuer profile components

---
+++ src/components/issuer/profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './profile.css';
+import NotIssuer from './notIssuer';
+import PendingIssuer from './pendingIssuer';
+import Issuer from './issuer';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+    User : state.User
+})
+
+const Profile = props => (
+    <div>
+        {props.User.info.issuer.status === "0"?
+            <NotIssuer address={props.User.info.address}/>
+            :
+            <div>
+                {props.User.info.issuer.status === "1"?
+                    <PendingIssuer/>
+                    :
+                    <Issuer/>
+                }
+            </div>
+        }
+    </div>
+)
+export default connect(mapStateToProps,null)(Profile);
---
+++ src/components/issuer/request.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import EachReq from './eachReq';
+
+const Request = props => (
+    <div className="request">
+        {props.data.length === 0?
+        <h4>No Requests Found</h4>
+        :
+        <div>
+            {props.data.map( each =>(
+                <EachReq req = {each} key = {each.requestNo} id={props.id}/>
+            ))}
+        </div>    
+        }
+    </div>
+)
+export default Request;
